9dc035aff43a995cdf01fc247c7bcc2ddee28ee9
Даниил Калинин <crm.v.28556@gmail.com>
2025-06-10 21:04:22 +0300

    Add swagger

--- MarckerPlaceBack/Program.cs
+++ MarckerPlaceBack/Program.cs
@@ -12,12 +12,17 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(); // You can configure options here if needed
 }
 
 app.UseHttpsRedirection();
