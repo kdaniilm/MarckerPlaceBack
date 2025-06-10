0e2ed9efa822ecc15ea6d97b9de2da8a6a8e9843
Даниил Калинин <crm.v.28556@gmail.com>
2025-06-10 20:55:38 +0300

    Add ClassLibraries

--- MarckerPlaceBack/Program.cs
+++ MarckerPlaceBack/Program.cs
@@ -1,6 +1,12 @@
+using MarckerPlaceBack.Core.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DbConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
