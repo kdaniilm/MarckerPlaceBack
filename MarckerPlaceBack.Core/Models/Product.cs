824283612b8c3f5739f6f19a8dc44298f943d67a
Даниил Калинин <crm.v.28556@gmail.com>
2025-06-10 21:52:01 +0300

    Configure Database and implement Migrations

---
+++ MarckerPlaceBack.Core/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarckerPlaceBack.Core.Models
+{
+    public class Product
+    {
+        [Key]
+        public long ProductId { get; set; }
+        public string Name { get; set; }
+        public int CategoryId { get; set; }
+        public string Articul { get; set; }
+
+        [Precision(18, 2)]
+        public decimal Price { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
---
+++ MarckerPlaceBack.Core/Models/Purchare.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarckerPlaceBack.Core.Models
+{
+    public class Purchare
+    {
+        [Key]
+        public long PurchareId { get; set; }
+        public DateTime PurchareDate { get; set; }
+        public string TotalPrice { get; set; }
+
+        public List<Product> Products { get; set; }
+    }
+}
